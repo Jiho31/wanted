c7c2696aed5a41deba658356c4024f3d3c35fb9d
Jiho Bok <bokjiho@Macs-MacBook-Air.local>
2022-01-11 17:17:47 +0900

    Feat: 라우팅 설정

--- project/src/App.js
+++ project/src/App.js
@@ -1,8 +1,12 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import MainPage from "./pages/MainPage";
+
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <BrowserRouter>
+      <Route exact path='/' component={MainPage} />
+    </BrowserRouter>
   );
 }
 
