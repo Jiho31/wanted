a610d9f33a553a4f001c09039fbffe91b6d5a711
Jiho Bok <bokjiho@gmail.com>
2022-01-15 13:13:25 +0900

    Add: 캐러셀에 버튼 컴포넌트 추가

--- project/src/components/Banner.jsx
+++ project/src/components/Banner.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import styled from "styled-components";
 
 import img1 from "../assets/images/banner1.jpg";
 import img2 from "../assets/images/banner2.jpg";
@@ -16,132 +17,217 @@ const bannerData = [
   {
     id: 1,
     title: "스타벅스 굿즈 좋아하세요?",
-    content: "사랑받는 디자인의 비밀",
+    subtitle: "사랑받는 디자인의 비밀",
+    image: img1,
   },
   {
     id: 2,
     title: "마케터를 위한 데이터",
-    content: "잘 나가는 마케터는 무엇이 다를까!?",
+    subtitle: "잘 나가는 마케터는 무엇이 다를까!?",
+    image: img2,
   },
   {
     id: 3,
     title: "요즘 MD가 일하는 방법",
-    content: "실무자가 공개하는 MD 커리어의 모든 것!",
+    subtitle: "실무자가 공개하는 MD 커리어의 모든 것!",
+    image: img3,
   },
   {
     id: 4,
     title: "개발자 성장 비결 공개!",
-    content: "글 쓰는 개발자들의 이야기",
+    subtitle: "글 쓰는 개발자들의 이야기",
+    image: img4,
   },
   {
     id: 5,
     title: "나도 개발자 되고 싶다",
-    content: "프론트엔드 무료 교육과정 참여하기",
+    subtitle: "프론트엔드 무료 교육과정 참여하기",
+    image: img5,
   },
   {
     id: 6,
     title: "리크루터 커리어 가이드",
-    content: "싼마이 리크루터가 되지 않기 위해",
+    subtitle: "싼마이 리크루터가 되지 않기 위해",
+    image: img6,
   },
   {
     id: 7,
     title: "유저 경험을 설계하라!",
-    content: "문제를 해결하는 프로덕트 디자인",
+    subtitle: "문제를 해결하는 프로덕트 디자인",
+    image: img7,
   },
   {
     id: 8,
     title: "아는 만큼 보인다!",
-    content: "노동법 '채용' 시리즈",
+    subtitle: "노동법 '채용' 시리즈",
+    image: img8,
   },
   {
     id: 9,
     title: "해, 커리어 EP 02 공개",
-    content: "최종 발표를 위한 마지막 관문 2라운드의 승자는?",
+    subtitle: "최종 발표를 위한 마지막 관문 2라운드의 승자는?",
+    image: img9,
   },
   {
     id: 10,
     title: "믿을 것은 데이터 뿐!",
-    content: "요즘 데이터팀은 어떻게 일할까?",
+    subtitle: "요즘 데이터팀은 어떻게 일할까?",
+    image: img10,
   },
   {
     id: 11,
     title: "Git? GitHub?",
-    content: "협업 필수 도구 마스터하기",
+    subtitle: "협업 필수 도구 마스터하기",
+    image: img11,
   },
 ];
 
+// styled components
+const StyledBanner = styled.div`
+  height: auto;
+  position: relative;
+  margin-top: 25px;
+`;
+const CarouselWrapper = styled.div`
+  width: calc(100vw * 11);
+  display: flex;
+  // overflow: hidden;
+`;
+const Carousel = styled.div`
+  width: auto;
+  height: auto;
+  display: inline-flexß;
+`;
+const CarouselContent = styled.article`
+  width: 1060px;
+  padding: 0 12px;
+  box-sizing: content-box;
+
+  img {
+    border-radius: 4px;
+  }
+`;
+
+const PrevArrow = styled.span``;
+const NextArrow = styled.span`
+  right: 0;
+  top: 40%;
+`;
+const BannerArrow = styled.button`
+  width: 30px;
+  height: 60px;
+  border-radius: 15px;
+  background-color: #fff;
+  opacity: 0.5;
+  font-size: 16px;
+
+  position: absolute;
+  top: 120px;
+
+  svg {
+    width: 16px;
+    height: 16px;
+  }
+
+  ${PrevArrow} {
+    left: calc((100% - 1210px) / 2);
+  }
+`;
+
+const BannerInfoBox = styled.div`
+  width: 330px;
+  height: 146px;
+  background-color: #fff;
+  border-radius: 4px;
+  position: absolute;
+  bottom: 28px;
+  left: 34px;
+  h2 {
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 1.5;
+    margin: 20px 20px 0 20px;
+  }
+  h3 {
+    height: 44px;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 1.64;
+    margin: 0 20px;
+    border-bottom: ;
+  }
+  hr {
+    border: 0;
+    height: 1px;
+    background-color: #ececec;
+  }
+`;
+const InfoLink = styled.a`
+  height: 40px;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 1;
+  display: inline-flex;
+  align-items: center;
+  margin: 6px 20px;
+
+  button {
+    color: #36f;
+    cursor: pointer;
+  }
+  svg {
+    width: 14px;
+    height: 14px;
+    fill: #36f;
+    line-height: 1;
+  }
+`;
+
+// Banner 컴포넌트
 const Banner = () => {
   return (
-    <section className="banner">
-      <div className="carousel-container">
-        <div>
-          <button>{"<"}</button>
-        </div>
+    <StyledBanner>
+      <CarouselWrapper>
+        <Carousel>
+          {bannerData.map((data) => {
+            const altText = `image${data.id}`;
 
-        <div className="carousel">
-          <article>
-            <img src={img1} alt="image1" />
-          </article>
-          <article>
-            <img src={img2} alt="image2" />
-          </article>
-          <article>
-            <img src={img3} alt="image3" />
-          </article>
-          <div>
-            <button>{">"}</button>
-          </div>
-          {/* <div>
-            <img
-              src="https://static.wanted.co.kr/images/banners/1460/619f3af7.jpg"
-              alt="image4"
-            />
-          </div>
-          <div>
-            <img
-              src="https://static.wanted.co.kr/images/banners/1473/41f7b36e.jpg"
-              alt="image5"
-            />
-          </div>
-          <div>
-            <img
-              src="https://static.wanted.co.kr/images/banners/1453/7a978579.jpg"
-              alt="image6"
-            />
-          </div>
-          <div>
-            <img
-              src="https://static.wanted.co.kr/images/banners/1435/6cdcea85.jpg"
-              alt="image7"
-            />
-          </div>
-          <div>
-            <img
-              src="https://static.wanted.co.kr/images/banners/1454/e504b006.jpg"
-              alt="image8"
-            />
-          </div>
-          <div>
-            <img
-              src="https://static.wanted.co.kr/images/banners/1468/3df61cbc.jpg"
-              alt="image9"
-            />
-          </div>
-          <div>
-            <img
-              src="https://static.wanted.co.kr/images/banners/1451/725c6862.jpg"
-              alt="image10"
-            />
-          </div>
-          <div>
-            <img
-              src="https://static.wanted.co.kr/images/banners/1452/be4ec643.jpg"
-              alt="image11"
-            />
-          </div> */}
-        </div>
-      </div>
-    </section>
+            return (
+              <CarouselContent>
+                <img src={data.image} alt={altText} />
+                <BannerInfoBox>
+                  <h2>{data.title}</h2>
+                  <h3>{data.subtitle}</h3>
+                  <hr />
+                  <div>
+                    <InfoLink href="#">
+                      <button>바로가기</button>
+                      <svg viewBox="0 0 18 18">
+                        <path d="m11.955 9-5.978 5.977a.563.563 0 0 0 .796.796l6.375-6.375a.563.563 0 0 0 0-.796L6.773 2.227a.562.562 0 1 0-.796.796L11.955 9z"></path>
+                      </svg>
+                    </InfoLink>
+                  </div>
+                </BannerInfoBox>
+              </CarouselContent>
+            );
+          })}
+        </Carousel>
+        <BannerArrow>
+          <PrevArrow>
+            <svg viewBox="0 0 16 16">
+              <path d="m6.045 9 5.978-5.977a.563.563 0 1 0-.796-.796L4.852 8.602a.562.562 0 0 0 0 .796l6.375 6.375a.563.563 0 0 0 .796-.796L6.045 9z"></path>
+            </svg>
+          </PrevArrow>
+        </BannerArrow>
+        <BannerArrow>
+          <NextArrow>
+            <svg viewBox="0 0 16 16">
+              <path d="m11.955 9-5.978 5.977a.563.563 0 0 0 .796.796l6.375-6.375a.563.563 0 0 0 0-.796L6.773 2.227a.562.562 0 1 0-.796.796L11.955 9z"></path>
+            </svg>
+          </NextArrow>
+        </BannerArrow>
+      </CarouselWrapper>
+    </StyledBanner>
   );
 };
 
--- project/src/pages/MainPage.jsx
+++ project/src/pages/MainPage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import Banner from "../components/Banner";
 
 const MainPage = () => {
   return (
     <main>
       <NavBar />
+      <Banner />
     </main>
   );
 };
