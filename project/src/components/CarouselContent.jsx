590b6f7e2f6782cb1cee31c51f06dccb95ea17fb
Jiho Bok <bokjiho@gmail.com>
2022-01-16 19:58:56 +0900

    Update: 캐러셀 내용 컴포넌트 별도의 파일로 분리

---
+++ project/src/components/CarouselContent.jsx
@@ -0,0 +1,91 @@
+import React from "react";
+import styled from "styled-components";
+
+const CarouselContent = ({ data }) => {
+  return (
+    <Content>
+      <img src={data.image} alt={`banner${data.id}`} />
+      <ContentInfo>
+        <h2>{data.title}</h2>
+        <h3>{data.subtitle}</h3>
+        <hr />
+        <div>
+          <InfoLink href="#">
+            <button>바로가기</button>
+            <svg viewBox="0 0 18 18">
+              <path d="m11.955 9-5.978 5.977a.563.563 0 0 0 .796.796l6.375-6.375a.563.563 0 0 0 0-.796L6.773 2.227a.562.562 0 1 0-.796.796L11.955 9z"></path>
+            </svg>
+          </InfoLink>
+        </div>
+      </ContentInfo>
+    </Content>
+  );
+};
+
+const Content = styled.article`
+  width: 1060px;
+  padding: 0 12px;
+  box-sizing: content-box;
+
+  img {
+    border-radius: 4px;
+  }
+`;
+const ContentInfo = styled.div`
+  width: 330px;
+  height: 146px;
+  background-color: #fff;
+  border-radius: 4px;
+  position: absolute;
+  bottom: 28px;
+  left: 34px;
+  opacity: 0;
+  ${(props) =>
+    props.visible &&
+    `
+    opacity: 1;
+    animation: 0.4s ease-in-out;
+  `}
+
+  h2 {
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 1.5;
+    margin: 20px 20px 0 20px;
+  }
+  h3 {
+    height: 44px;
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 1.64;
+    margin: 0 20px;
+    border-bottom: ;
+  }
+  hr {
+    border: 0;
+    height: 1px;
+    background-color: #ececec;
+  }
+`;
+const InfoLink = styled.a`
+  height: 40px;
+  font-size: 14px;
+  font-weight: 700;
+  line-height: 1;
+  display: inline-flex;
+  align-items: center;
+  margin: 6px 20px;
+
+  button {
+    color: #36f;
+    cursor: pointer;
+  }
+  svg {
+    width: 14px;
+    height: 14px;
+    fill: #36f;
+    line-height: 1;
+  }
+`;
+
+export default CarouselContent;
