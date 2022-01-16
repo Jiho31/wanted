24cac23b822c7d45e50e578b83bc37c04a8e191e
Jiho Bok <bokjiho@gmail.com>
2022-01-17 01:26:09 +0900

    Add: 무한 캐러셀 이벤트 추가

--- project/src/components/Banner.jsx
+++ project/src/components/Banner.jsx
@@ -1,5 +1,6 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import styled from "styled-components";
+import CarouselContent from "./CarouselContent";
 
 import img1 from "../assets/images/banner1.jpg";
 import img2 from "../assets/images/banner2.jpg";
@@ -15,70 +16,94 @@ import img11 from "../assets/images/banner11.jpg";
 
 const bannerData = [
   {
-    id: 1,
-    title: "스타벅스 굿즈 좋아하세요?",
-    subtitle: "사랑받는 디자인의 비밀",
-    image: img1,
-  },
-  {
-    id: 2,
-    title: "마케터를 위한 데이터",
-    subtitle: "잘 나가는 마케터는 무엇이 다를까!?",
-    image: img2,
-  },
-  {
-    id: 3,
-    title: "요즘 MD가 일하는 방법",
-    subtitle: "실무자가 공개하는 MD 커리어의 모든 것!",
-    image: img3,
-  },
-  {
-    id: 4,
-    title: "개발자 성장 비결 공개!",
-    subtitle: "글 쓰는 개발자들의 이야기",
-    image: img4,
-  },
-  {
-    id: 5,
-    title: "나도 개발자 되고 싶다",
-    subtitle: "프론트엔드 무료 교육과정 참여하기",
-    image: img5,
-  },
-  {
-    id: 6,
-    title: "리크루터 커리어 가이드",
-    subtitle: "싼마이 리크루터가 되지 않기 위해",
-    image: img6,
-  },
-  {
-    id: 7,
-    title: "유저 경험을 설계하라!",
-    subtitle: "문제를 해결하는 프로덕트 디자인",
-    image: img7,
-  },
-  {
-    id: 8,
-    title: "아는 만큼 보인다!",
-    subtitle: "노동법 '채용' 시리즈",
-    image: img8,
-  },
-  {
-    id: 9,
-    title: "해, 커리어 EP 02 공개",
-    subtitle: "최종 발표를 위한 마지막 관문 2라운드의 승자는?",
-    image: img9,
-  },
-  {
-    id: 10,
+    id: -2,
     title: "믿을 것은 데이터 뿐!",
     subtitle: "요즘 데이터팀은 어떻게 일할까?",
     image: img10,
   },
   {
-    id: 11,
+    id: -1,
     title: "Git? GitHub?",
     subtitle: "협업 필수 도구 마스터하기",
     image: img11,
+  },
+  {
+    id: 0,
+    title: "스타벅스 굿즈 좋아하세요?",
+    subtitle: "사랑받는 디자인의 비밀",
+    image: img1,
+  },
+  {
+    id: 1,
+    title: "마케터를 위한 데이터",
+    subtitle: "잘 나가는 마케터는 무엇이 다를까!?",
+    image: img2,
+  },
+  {
+    id: 2,
+    title: "요즘 MD가 일하는 방법",
+    subtitle: "실무자가 공개하는 MD 커리어의 모든 것!",
+    image: img3,
+  },
+  {
+    id: 3,
+    title: "개발자 성장 비결 공개!",
+    subtitle: "글 쓰는 개발자들의 이야기",
+    image: img4,
+  },
+  {
+    id: 4,
+    title: "나도 개발자 되고 싶다",
+    subtitle: "프론트엔드 무료 교육과정 참여하기",
+    image: img5,
+  },
+  {
+    id: 5,
+    title: "리크루터 커리어 가이드",
+    subtitle: "싼마이 리크루터가 되지 않기 위해",
+    image: img6,
+  },
+  {
+    id: 6,
+    title: "유저 경험을 설계하라!",
+    subtitle: "문제를 해결하는 프로덕트 디자인",
+    image: img7,
+  },
+  {
+    id: 7,
+    title: "아는 만큼 보인다!",
+    subtitle: "노동법 '채용' 시리즈",
+    image: img8,
+  },
+  {
+    id: 8,
+    title: "해, 커리어 EP 02 공개",
+    subtitle: "최종 발표를 위한 마지막 관문 2라운드의 승자는?",
+    image: img9,
+  },
+  {
+    id: 9,
+    title: "믿을 것은 데이터 뿐!",
+    subtitle: "요즘 데이터팀은 어떻게 일할까?",
+    image: img10,
+  },
+  {
+    id: 10,
+    title: "Git? GitHub?",
+    subtitle: "협업 필수 도구 마스터하기",
+    image: img11,
+  },
+  {
+    id: 11,
+    title: "스타벅스 굿즈 좋아하세요?",
+    subtitle: "사랑받는 디자인의 비밀",
+    image: img1,
+  },
+  {
+    id: 12,
+    title: "마케터를 위한 데이터",
+    subtitle: "잘 나가는 마케터는 무엇이 다를까!?",
+    image: img2,
   },
 ];
 
@@ -89,25 +114,21 @@ const StyledBanner = styled.div`
   margin-top: 25px;
 `;
 const CarouselWrapper = styled.div`
-  width: 100vw;
+  width: 100%;
   display: flex;
   overflow: hidden;
+  position: relative;
 `;
 const Carousel = styled.div`
   // width: calc(100vw * 11);
-  width: 100vw;
+  width: 100%;
   height: auto;
   display: inline-flex;
+  // transition: all 0.25s linear;
+  transition: all 0s;
+  transform: translateX(2168px);
 `;
-const CarouselContent = styled.article`
-  width: 1060px;
-  padding: 0 12px;
-  box-sizing: content-box;
 
-  img {
-    border-radius: 4px;
-  }
-`;
 const BannerArrow = styled.button`
   width: 30px;
   height: 60px;
@@ -136,132 +157,73 @@ const BannerArrow = styled.button`
   `}
 `;
 
-const ContentInfo = styled.div`
-  width: 330px;
-  height: 146px;
-  background-color: #fff;
-  border-radius: 4px;
-  position: absolute;
-  bottom: 28px;
-  left: 34px;
-  opacity: 0;
-  ${(props) =>
-    props.visible &&
-    `
-    opacity: 1;
-    animation: 0.4s ease-in-out;
-  `}
-
-  h2 {
-    font-size: 20px;
-    font-weight: 700;
-    line-height: 1.5;
-    margin: 20px 20px 0 20px;
-  }
-  h3 {
-    height: 44px;
-    font-size: 14px;
-    font-weight: 500;
-    line-height: 1.64;
-    margin: 0 20px;
-    border-bottom: ;
-  }
-  hr {
-    border: 0;
-    height: 1px;
-    background-color: #ececec;
-  }
-`;
-const InfoLink = styled.a`
-  height: 40px;
-  font-size: 14px;
-  font-weight: 700;
-  line-height: 1;
-  display: inline-flex;
-  align-items: center;
-  margin: 6px 20px;
-
-  button {
-    color: #36f;
-    cursor: pointer;
-  }
-  svg {
-    width: 14px;
-    height: 14px;
-    fill: #36f;
-    line-height: 1;
-  }
-`;
-
 // Banner 컴포넌트
 const Banner = () => {
   const TOTAL_SLIDES = 11;
-  const [currentSlide, setCurrentSlide] = useState(1);
+  const [currentSlideIdx, setCurrentSlideIdx] = useState(0);
+
+  const [transitionEnabled, setTransitionEnabled] = useState(true);
+
   const sliderRef = useRef(null);
   const infoRef = useRef(null);
 
-  const nextSlide = () => {
-    if (currentSlide >= TOTAL_SLIDES) {
-      // 11 번째 슬라이드 이미지일 경우
-      // transition으로 다시 0 번째 슬라이드 이미지로 대체해서 보여주기
-      // setCurrentSlide(1);
-      return;
-    } else {
-      setCurrentSlide((prev) => prev + 1);
+  // useEffect(() => {
+  //   if (currentSlideIdx === TOTAL_SLIDES - 1 || currentSlideIdx === 0) {
+  //     setTransitionEnabled(true);
+  //   }
+  // }, [currentSlideIdx]);
+  const prevSlide = useCallback(() => {
+    if (currentSlideIdx > -1) {
+      setCurrentSlideIdx((prev) => prev - 1);
     }
-  };
-  const prevSlide = () => {
-    if (currentSlide === 1) {
-      // 첫 번째 슬라이드 이미지일 경우
-      // transition으로 마지막 슬라이드 이미지로 대체해서 보여주기
-      // setCurrentSlide(TOTAL_SLIDES);
-      return;
-    } else {
-      setCurrentSlide((prev) => prev - 1);
+  }, [currentSlideIdx]);
+  const nextSlide = useCallback(() => {
+    if (currentSlideIdx < TOTAL_SLIDES) {
+      setCurrentSlideIdx((prev) => prev + 1);
     }
-  };
+  }, [currentSlideIdx]);
+
+  const handleTransitionEnd = useCallback(() => {
+    if (currentSlideIdx === -1) {
+      setTransitionEnabled(false);
+
+      setCurrentSlideIdx(TOTAL_SLIDES - 1);
+    } else if (currentSlideIdx === TOTAL_SLIDES) {
+      setTransitionEnabled(false);
+      setCurrentSlideIdx(0);
+    }
+  }, [currentSlideIdx]);
 
   useEffect(() => {
-    console.log(`현재 슬라이드 인덱스: ${currentSlide}`);
+    console.log(`현재 슬라이드 인덱스: ${currentSlideIdx}`);
 
     // 배너 이미지 슬라이드
-    sliderRef.current.style.transition = "all 0.4s ease-out";
-    // sliderRef.current.style.transform = `translateX(-${currentSlide - 1}00%)`;
+    // sliderRef.current.style.transform = `translateX(-${currentSlideIdx - 1}00%)`;
     sliderRef.current.style.transform = `translateX(-${
-      (currentSlide - 1) * 1084
+      (currentSlideIdx + 2) * 1084
     }px)`;
+    // sliderRef.current.style.transition = "all 0.25s linear";
+    sliderRef.current.style.transition = !transitionEnabled
+      ? "none"
+      : "all 0.25s linear";
+    if (!transitionEnabled) {
+      if (currentSlideIdx === TOTAL_SLIDES - 1 || currentSlideIdx === 0) {
+        setTransitionEnabled(true);
+      }
+    }
 
-    // 배너 설명 컴포넌트 보이고 숨기기
-    infoRef.current.style.animation = "0.4s ease-in-out";
-    infoRef.current.style.opacity = 1;
+    // 배너 설명 컴포넌트 보이고 숨기기 <수정 필요!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!>
+    // infoRef.current.style.animation = "0.4s ease-in-out";
+    // infoRef.current.style.opacity = 1;
     // infoRef.current.classList.add("visible");
-  }, [currentSlide]);
+  }, [currentSlideIdx, transitionEnabled]);
 
   return (
     <StyledBanner>
       <CarouselWrapper>
-        <Carousel ref={sliderRef}>
+        <Carousel ref={sliderRef} onTransitionEnd={handleTransitionEnd}>
           {bannerData.map((data) => {
-            const altText = `image${data.id}`;
-
-            return (
-              <CarouselContent>
-                <img src={data.image} alt={altText} />
-                <ContentInfo ref={infoRef}>
-                  <h2>{data.title}</h2>
-                  <h3>{data.subtitle}</h3>
-                  <hr />
-                  <div>
-                    <InfoLink href="#">
-                      <button>바로가기</button>
-                      <svg viewBox="0 0 18 18">
-                        <path d="m11.955 9-5.978 5.977a.563.563 0 0 0 .796.796l6.375-6.375a.563.563 0 0 0 0-.796L6.773 2.227a.562.562 0 1 0-.796.796L11.955 9z"></path>
-                      </svg>
-                    </InfoLink>
-                  </div>
-                </ContentInfo>
-              </CarouselContent>
-            );
+            return <CarouselContent data={data} key={data.id} />;
           })}
         </Carousel>
         <BannerArrow onClick={prevSlide} prev>
